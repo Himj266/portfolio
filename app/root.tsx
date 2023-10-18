9e28120e3e48f26444ae785fd7c19c366eb9d76d
Himanshu Jain <jainh246@gmail.com>
2023-10-18 19:44:59 +0530

    feat: inital ui

--- app/root.tsx
+++ app/root.tsx
@@ -4,16 +4,16 @@ import {
   Meta,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react';
 
 //types
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction } from '@remix-run/node';
 
 //css
-import stylesheet from "~/tailwind.css";
+import stylesheet from '~/tailwind.css';
 
 export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: stylesheet },
+  { rel: 'stylesheet', href: stylesheet },
 ];
 
 export default function App() {
@@ -26,7 +26,24 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <div className="text-3xl font-bold underline">Test</div>
+        <div className="bg-slate-800">
+          <div className="flex">
+            <div className="h-screen border-r-2 border-gray-400 p-2">left</div>
+            <div className="flex h-screen grow flex-col p-2">
+              <div className="grow text-8xl font-black text-gray-100">
+                Canvas For Ideas To Paint
+              </div>
+              <div className="flex flex-col items-end text-right text-gray-200">
+                <div className="text-3xl">The Artist</div>
+                <div className="w-1/2 text-lg">
+                  Himanshu Jain, working as frontend developer at Sprinklr, tech
+                  enthusiast, who loves to play sport especially badminton,
+                  dance, learn new things and explore
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
