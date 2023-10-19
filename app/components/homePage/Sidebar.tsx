7ecbdb29f800514ef258e3e0040903b8171f2b86
Himanshu Jain <jainh246@gmail.com>
2023-10-20 03:43:33 +0530

    remove prettier plugin

--- app/components/homePage/Sidebar.tsx
+++ app/components/homePage/Sidebar.tsx
@@ -1,3 +1,10 @@
+//components
+
 export const Sidebar = (): JSX.Element => (
-  <div className="h-screen border-r-2 border-gray-400 p-2">left</div>
+  <div className="h-screen border-r-2 border-gray-400 p-2">
+    <div className="flex flex-col gap-2">
+      {/* // <SocialIcon url="https://twitter.com/DeMonk26" /> */}
+      {/* <SocialIcon url="https://www.instagram.com/himj26/" /> */}
+    </div>
+  </div>
 );
