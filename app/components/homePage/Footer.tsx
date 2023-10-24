df44df2a044f32437c83876092001e7904726f4b
Himanshu Jain <jainh246@gmail.com>
2023-10-25 01:38:04 +0530

    fix styles

--- app/components/homePage/Footer.tsx
+++ app/components/homePage/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer = (): JSX.Element => (
   <div className="flex flex-col items-end text-right text-gray-200">
-    <div className="text-3xl">The Artist</div>
-    <div className="w-1/2 text-lg">
+    <div className="text-6xl">The Artist</div>
+    <div className="w-1/2 text-3xl">
       Himanshu Jain, working as frontend developer at Sprinklr, tech enthusiast,
       who loves to play sport especially badminton, dance, learn new things and
       explore
--- app/components/homePage/Sidebar.tsx
+++ app/components/homePage/Sidebar.tsx
@@ -2,10 +2,13 @@
 import { SocialIcon } from 'react-social-icons';
 
 export const Sidebar = (): JSX.Element => (
-  <div className="h-screen border-r-2 border-gray-400 p-2">
+  <div className="h-screen border-r-2 border-gray-400 p-2 pt-4">
     <div className="flex flex-col gap-2">
-      <SocialIcon url="https://x.com/DeMonk26" />
-      <SocialIcon url="https://www.instagram.com/himj26/" />
+      <SocialIcon url="https://x.com/DeMonk26" bgColor="transparent" />
+      <SocialIcon
+        url="https://www.instagram.com/himj26/"
+        bgColor="transparent"
+      />
     </div>
   </div>
 );
