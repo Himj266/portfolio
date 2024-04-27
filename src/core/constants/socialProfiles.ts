b2ef74744f74a1bfdab019296f22f2d11de0063e
Himanshu Jain <jainh246@gmail.com>
2024-04-27 23:28:30 +0530

    add labels in icons

--- src/core/constants/socialProfiles.ts
+++ src/core/constants/socialProfiles.ts
@@ -1,4 +1,4 @@
 export const SOCIAL_PROFILES = [
-  { url: "https://www.instagram.com/himj26/", type: "INSTAGRAM" },
-  { url: "https://twitter.com/himJ26", type: "TWIITER" },
+  { url: "https://www.instagram.com/himj26/", label: "Instagram" },
+  { url: "https://twitter.com/himJ26", label: "Twitter" },
 ];
