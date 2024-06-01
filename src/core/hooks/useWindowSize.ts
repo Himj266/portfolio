2aee7596badc1de5702893b9c1546540d41a4997
Himanshu Jain <jainh246@gmail.com>
2024-06-02 00:16:43 +0530

    add useWindowSize hook

---
+++ src/core/hooks/useWindowSize.ts
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+//utils
+import { isBrowser } from "@/core/utils/isBrowser";
+
+export const useWindowSize = (): {
+  width: number | undefined;
+  height: number | undefined;
+} => {
+  const [{ width, height }, setWindowSize] = useState({
+    width: isBrowser ? window.innerWidth : undefined,
+    height: isBrowser ? window.innerHeight : undefined,
+  });
+
+  useEffect(() => {
+    if (isBrowser) {
+      const handler = () =>
+        setWindowSize(() => ({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        }));
+
+      window.addEventListener("resize", handler);
+      window.removeEventListener("resize", handler);
+    }
+  }, []);
+
+  return { width, height };
+};
